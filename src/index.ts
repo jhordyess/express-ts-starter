438a15fed01b5865a5c388871d9cd7a042f0bfbc
jhordyess <65508346+jhordyess@users.noreply.github.com>
2025-11-12 12:03:41 -0400

    fix: update log message for server startup to provide clearer information

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@ app.get('/', (_, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.info(`Server is running on http://localhost:${port}`)
 })
