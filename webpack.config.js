14fc09d5a9ca767808b9a8dc5a59ec920fe9f0ce
jhordyess <65508346+jhordyess@users.noreply.github.com>
2025-11-12 11:46:59 -0400

    feat: enhance webpack configuration with nodemon and dotenv support
    - Added nodemon-webpack-plugin to automatically restart the server on changes.
    - Integrated dotenv-webpack to manage environment variables from a .env file.
    - Updated the mode in the webpack configuration to use NODE_ENV for better environment handling.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 import path from 'node:path'
 import nodeExternals from 'webpack-node-externals'
+import nodemonPlugin from 'nodemon-webpack-plugin'
+import dotenvWebpack from 'dotenv-webpack'
+
+const NODE_ENV = process.env.NODE_ENV
 
 /** @type {import('webpack').Configuration} */
 export default {
@@ -7,7 +11,7 @@ export default {
     index: path.resolve('src', 'index.ts')
   },
   target: 'node',
-  mode: 'production',
+  mode: NODE_ENV,
   externals: [nodeExternals()],
   output: {
     path: path.resolve('dist'),
@@ -31,5 +35,12 @@ export default {
       }
     ]
   },
-  plugins: []
+  plugins: [
+    new nodemonPlugin({
+      script: path.resolve('dist', 'index.cjs')
+    }),
+    new dotenvWebpack({
+      systemvars: true
+    })
+  ]
 }
